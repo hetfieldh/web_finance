7323963301460ff167df854399bbfd826958d8d8
Douglas Fonseca <osorio.douglas@outlook.com.br>
2025-11-16 16:59:32 -0300

    Versão Beta 1.8 - Implementação de classificação por coluna nas tabelas

--- app/static/js/global.js
+++ app/static/js/global.js
@@ -56,3 +56,15 @@ function inicializarDatepickerMesAno(selector, onChangeCallback) {
       });
   }
 }
+
+$(document).ready(function () {
+  $(".sortable-table").DataTable({
+    language: {
+      url: "//cdn.datatables.net/plug-ins/1.13.6/i18n/pt-BR.json",
+    },
+    paging: false,
+    searching: false,
+    info: false,
+    responsive: true,
+  });
+});
